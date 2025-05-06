66ca548caab7b4480a5946f9571da237d2834f81
loveoopi <143441383+loveoopi@users.noreply.github.com>
2025-05-07 00:33:07 +0530

    config.py

---
+++ config.py
@@ -0,0 +1,10 @@
+# Telegram API credentials
+API_ID = 20284828
+API_HASH = 'a980ba25306901d5c9b899414d6a9ab7'
+BOT_TOKEN = '8013483965:AAGZP0Ji_ZDGgJ5uoluyThj6SkVSneCHO9Y'
+
+# Developer information
+DEVELOPER = "@kingXkingz"
+
+# Bot settings
+BOT_NAME = "Bio Link Protector Bot"
