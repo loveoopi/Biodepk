68ed2ee9cec1fd47dcd793d995f2ba2288784ccf
loveoopi <143441383+loveoopi@users.noreply.github.com>
2025-05-07 00:57:18 +0530

    config.py

--- config.py
+++ config.py
@@ -4,7 +4,7 @@ API_HASH = 'a980ba25306901d5c9b899414d6a9ab7'
 BOT_TOKEN = '8013483965:AAGZP0Ji_ZDGgJ5uoluyThj6SkVSneCHO9Y'
 
 # Developer information
-DEVELOPER = "@kingXkingz"
+DEVELOPER = "https://t.me/kingXkingz"
 
 # Bot settings
 BOT_NAME = "Bio Link Protector Bot"
