c500c8c07d3aca37f596dc0f98a1123527da19f5
loveoopi <143441383+loveoopi@users.noreply.github.com>
2025-05-07 01:17:38 +0530

    config.py

--- config.py
+++ config.py
@@ -1,7 +1,7 @@
 # Telegram API credentials
 API_ID = 20284828
 API_HASH = 'a980ba25306901d5c9b899414d6a9ab7'
-BOT_TOKEN = '8013483965:AAGZP0Ji_ZDGgJ5uoluyThj6SkVSneCHO9Y'
+BOT_TOKEN = '8006838722:AAF7tqCAlnj6oDh3ALAudUxr99UQbTwUlv4'
 
 # Developer information
 DEVELOPER = "https://t.me/kingXkingz"
